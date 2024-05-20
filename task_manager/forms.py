1443a16d018167f8b0283876944fdaf1b21825ab
Artem Andriushchenko <link.by.artem@gmail.com>
2024-05-20 22:47:47 +0300

    create forms to update and create workers

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -3,10 +3,26 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
-from task_manager.models import Position
+from task_manager.models import Position, Worker
 
 
 class PositionForm(forms.ModelForm):
     class Meta:
         model = Position
+        fields = "__all__"
+
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "position",
+        )
+
+
+class WorkerUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Worker
         fields = "__all__"
