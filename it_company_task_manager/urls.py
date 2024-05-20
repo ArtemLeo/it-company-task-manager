420b1de76d841669ed0fc61a19c1b64035637793
Artem Andriushchenko <link.by.artem@gmail.com>
2024-05-20 22:02:59 +0300

    create paths for task type list, create, update and delete

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,35 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    TaskTypeListView,
+    TaskTypeCreateView,
+    TaskTypeUpdateView,
+    TaskTypeDeleteView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "task_types/",
+        TaskTypeListView.as_view(),
+        name="task-type-list",
+    ),
+    path(
+        "task_types/create/",
+        TaskTypeCreateView.as_view(),
+        name="task-type-create",
+    ),
+    path(
+        "task_types/<int:pk>/update/",
+        TaskTypeUpdateView.as_view(),
+        name="task-type-update",
+    ),
+    path(
+        "task_types/<int:pk>/delete/",
+        TaskTypeDeleteView.as_view(),
+        name="task-type-delete",
+    ),
 ]
 
 app_name = "task_manager"
