16fa89070017f30bfe2008305bb45fd4a181d257
Artem Andriushchenko <link.by.artem@gmail.com>
2024-05-20 22:08:41 +0300

    create CRUD classes for Task Type model

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from task_manager.models import TaskType, Position, Worker
 
@@ -18,3 +21,28 @@ def index(request):
     }
 
     return render(request, "task_manager/index.html", context=context)
+
+
+class TaskTypeListView(LoginRequiredMixin, generic.ListView):
+    model = TaskType
+    context_object_name = "task_type_list"
+    template_name = "task_manager/task_type_list.html"
+    paginate_by = 10
+    queryset = TaskType.objects.select_related("tasks")
+
+
+class TaskTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
+class TaskTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
+class TaskTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = TaskType
+    success_url = reverse_lazy("task_manager:task-type-list")
