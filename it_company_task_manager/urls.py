cb0aff17dd2a1216c3bc36d55753e4e2de11841a
Artem Andriushchenko <link.by.artem@gmail.com>
2024-05-20 19:22:16 +0300

    add user authentication

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
