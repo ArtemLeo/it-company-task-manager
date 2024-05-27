73a8c940c1fbf038dcbe3ec9402c7080b77a993c
Artem Andriushchenko <link.by.artem@gmail.com>
2024-05-27 19:04:29 +0300

    create static directory and add it to settings

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap4",
     "task_manager",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -123,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("task_manager.urls", namespace="task_manager")),
-    path("accounts/", include("django.contrib.auth.urls")),
-]
+                  path("admin/", admin.site.urls),
+                  path("", include("task_manager.urls", namespace="task_manager")),
+                  path("accounts/", include("django.contrib.auth.urls")),
+                  path("__debug__/", include("debug_toolbar.urls")),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
