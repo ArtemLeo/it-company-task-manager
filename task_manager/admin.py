bc2fa235f77598c2a753e2b0d355d00e87d81e5b
Artem Andriushchenko <link.by.artem@gmail.com>
2024-05-27 18:47:52 +0300

    register models in admin page

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_manager.models import Worker, Task, TaskType, Position
+
+
+@admin.register(Worker)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("priority",)
+
+
+admin.site.register(TaskType)
+admin.site.register(Position)
