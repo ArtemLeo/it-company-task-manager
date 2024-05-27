700403d9f092d332a8d1a013a408ce7de4dae9f4
Artem Andriushchenko <link.by.artem@gmail.com>
2024-05-27 20:05:00 +0300

    assign ASSETS_ROOT as /static/assets

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "task_manager.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -129,6 +130,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
