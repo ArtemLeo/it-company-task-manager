f1dc822a6c8c5a950300439a94d46e0ff62b58c2
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-01 04:57:54 +0300

    add link project to ALLOWED_HOSTS

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://it-company-task-manager-project.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "https://it-company-task-manager-pwni.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
