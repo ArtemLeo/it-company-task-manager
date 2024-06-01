12d6e3ba2e490add7385574db7313ac91b753a54
Artem Andriushchenko <link.by.artem@gmail.com>
2024-06-01 05:57:56 +0300

    fixed path it-company-task-manager-pwni.onrender.com in ALLOWED_HOSTS

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["localhost", "https://it-company-task-manager-pwni.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "it-company-task-manager-pwni.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
